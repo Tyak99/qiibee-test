6b1c1b9d97b522a3455a7d213cdda08f42150797
Tunde Nasri <tundenasri@gmail.com>
2021-05-27 23:46:35 +0100

    show welcome card in brands page

--- src/pages/Brands.js
+++ src/pages/Brands.js
@@ -1,5 +1,6 @@
 import {
   Box, Heading, HStack, Text,
+  Flex,
 } from '@chakra-ui/layout';
 import React from 'react';
 import { Select } from '@chakra-ui/select';
@@ -10,7 +11,23 @@ import Container from '../components/Container';
 const Brands = () => (
   <Container>
     {/* <Stats /> */}
-    <Heading px={{ base: '4', lg: 0 }} size="md">Welcome, Babatunde</Heading>
+    <Heading px={{ base: '4', lg: 0 }} size="md">Dashboard</Heading>
+    <Box mt="8" shadow="lg" borderRadius="lg" bg="white" p="6" mx={{ base: '4', lg: 0 }}>
+      <Flex justifyContent="space-between">
+        <Box>
+          <Text>Welcome,</Text>
+          <Text>Babatunde Yakub</Text>
+        </Box>
+        <Box>
+          <Text>Loyalty point</Text>
+          <Text>0</Text>
+        </Box>
+        <Box>
+          <Text>Followed Brands</Text>
+          <Text>0</Text>
+        </Box>
+      </Flex>
+    </Box>
     <Box mt="16">
       <Box px={{ base: '4', lg: 0 }}>
         <Heading size="md">Redeem Loyalty Tokens</Heading>
