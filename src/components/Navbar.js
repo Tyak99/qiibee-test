571349f7582561b792083d7f5a2d2b533aaf550a
Tunde Nasri <tundenasri@gmail.com>
2021-05-30 19:35:03 +0100

    implement log out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,43 +2,53 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Avatar } from '@chakra-ui/avatar';
 import {
-  Box, Flex, Heading, HStack, Link,
+  Box, Flex, Heading, HStack, Link, Text,
 } from '@chakra-ui/layout';
 import { Route as RouterLink } from 'react-router-dom';
+import { Button } from '@chakra-ui/button';
+import { useDispatch } from 'react-redux';
+import { authActions } from '../store/reducers/authReducer';
 
-const Navbar = (props) => (
-  <Flex
-    justifyContent="space-between"
-    p={['4', '8']}
-    position="fixed"
-    height="20"
-    top="0"
-    left="0"
-    right="0"
-    alignItems="center"
-    zIndex="1"
-    bg="white"
-    shadow="lg"
-  >
-    <Box>
-      <Heading size="md" color="teal" cursor="pointer">
-        <Link as={RouterLink} to="/login">LoyalityFx</Link>
-      </Heading>
-    </Box>
-    <Flex>
-      <HStack spacing="4">
-        <Link href="/profile">
-          <Avatar
-            name="Tunde Nasri"
-            size="sm"
-            cursor="pointer"
-            src="http://res.cloudinary.com/buymezobo/image/upload/v1622232510/qiibee/v5xpjizpvfsqm0axtkc2.svg"
-          />
-        </Link>
-      </HStack>
+const Navbar = (props) => {
+  const dispatch = useDispatch();
+  const logOut = () => {
+    dispatch(authActions.logOut());
+  };
+  return (
+    <Flex
+      justifyContent="space-between"
+      p={['4', '8']}
+      position="fixed"
+      height="20"
+      top="0"
+      left="0"
+      right="0"
+      alignItems="center"
+      zIndex="1"
+      bg="white"
+      shadow="lg"
+    >
+      <Box>
+        <Heading size="md" color="teal" cursor="pointer">
+          <Link as={RouterLink} to="/login">LoyalityFx</Link>
+        </Heading>
+      </Box>
+      <Flex>
+        <HStack spacing="4">
+          <Button variant="link" onClick={logOut}>Log Out</Button>
+          <Link href="/profile">
+            <Avatar
+              name="Tunde Nasri"
+              size="sm"
+              cursor="pointer"
+              src="http://res.cloudinary.com/buymezobo/image/upload/v1622232510/qiibee/v5xpjizpvfsqm0axtkc2.svg"
+            />
+          </Link>
+        </HStack>
+      </Flex>
     </Flex>
-  </Flex>
-);
+  );
+};
 
 Navbar.propTypes = {
 
